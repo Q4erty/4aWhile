4f93f6829dd225df75b0af48be6a450902ec0dfb
Q4erty <akkemer05@gmail.com>
2025-02-13 11:27:16 +0500

    third commit

--- src/Main.java
+++ src/Main.java
@@ -14,22 +14,21 @@ public class Main {
         Storage<DVD> dvdStorage = new InMemoryStorage<>();
 
         Book book1 = new Book(1, "War and Peace");
-        Book book2 = new Book(2, "The Great Gatsby");
-        Book book3 = new Book(3, "Crime and Punishment");
+        Book book2 = new Book(2, "Crime and Punishment");
 
-        Magazine magazine1 = new Magazine(4, "National Geographic");
-        Magazine magazine2 = new Magazine(5, "Wall-Street");
+        Magazine magazine1 = new Magazine(3, "National Geographic");
+        Magazine magazine2 = new Magazine(4, "Wall-Street");
 
-        DVD dvd1 = new DVD(6, "The Lord of the Rings");
-        DVD dvd2 = new DVD(7, "Inception");
+        DVD dvd1 = new DVD(5, "The Lord of the Rings");
+        DVD dvd2 = new DVD(6, "Harry Potter");
 
         Librarian librarian = new Librarian("Yerlan");
         Admin admin = new Admin("Aisulu");
         Student student = new Student("Nurzhan");
 
+        System.out.println("Adding new items: \n");
         librarian.addBook(bookStorage, book1);
         librarian.addBook(bookStorage, book2);
-        librarian.addBook(bookStorage, book3);
 
         librarian.addBook(magazineStorage, magazine1);
         librarian.addBook(magazineStorage, magazine2);
@@ -37,18 +36,21 @@ public class Main {
         librarian.addBook(dvdStorage, dvd1);
         librarian.addBook(dvdStorage, dvd2);
 
+        System.out.println("\n\nBorrowing book: \n");
         student.borrowBook(book1);
         student.borrowBook(book1);
 
+        System.out.println("\n\nReturning book: \n");
         student.returnBook(book1);
         student.returnBook(book1);
 
-        System.out.println("All books in storage:");
+        System.out.println("\n\nAll books in storage:\n");
         bookStorage.findAll().forEach(book -> System.out.println(book.getTitle()));
 
+        System.out.println("\n\nRemoving a book:\n");
         librarian.removeBook(bookStorage, 2);
 
-        System.out.println("All books after removal:");
+        System.out.println("\n\nAll books after remove:\n");
         bookStorage.findAll().forEach(book -> System.out.println(book.getTitle()));
     }
 }
